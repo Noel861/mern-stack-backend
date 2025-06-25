696a062ce7c88de384959cfbec3b3d5515ee0b1a
Noel861 <167622052+Noel861@users.noreply.github.com>
2025-06-25 18:49:46 +0500

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -31,6 +31,6 @@ if (process.env.NODE_ENV === "production") {
 connectDB();
 
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server started on port ${port}`);
 });
